9687dd622f7403e83d384d2b626f4e3f502895dc
Gabriel Gheorghe <knoppy273@live.com>
2020-08-04 18:57:08 +0300

    #24 Implement ASTNodeAssign

---
+++ Engine/Lang/AST/Nodes/ASTNodeAssign.cpp
@@ -0,0 +1,24 @@
+/*****************************************************************************************************************************
+ * Copyright 2020 Gabriel Gheorghe. All rights reserved.
+ * This code is licensed under the BSD 3-Clause "New" or "Revised" License
+ * License url: https://github.com/GabyForceQ/PolluxEngine/blob/master/LICENSE
+ *****************************************************************************************************************************/
+
+#include "Engine/enginepch.hpp"
+
+#include "ASTNodeAssign.hpp"
+
+namespace Pollux::Lang
+{
+	ASTNodeAssign::ASTNodeAssign(Token token) noexcept
+		:
+		ASTNodeBase{ std::move(token) }
+	{
+		ASTNodeAssign::RegisterType("ASTNodeAssign", +ASTNodeKind::ASTNodeAssign);
+	}
+
+	void ASTNodeAssign::Accept(IASTNodeVisitor* pVisitor)
+	{
+		return pVisitor->Visit(this);
+	}
+}
---
+++ Engine/Lang/AST/Nodes/ASTNodeAssign.hpp
@@ -0,0 +1,24 @@
+/*****************************************************************************************************************************
+ * Copyright 2020 Gabriel Gheorghe. All rights reserved.
+ * This code is licensed under the BSD 3-Clause "New" or "Revised" License
+ * License url: https://github.com/GabyForceQ/PolluxEngine/blob/master/LICENSE
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#include "../ASTNodeBase.hpp"
+
+namespace Pollux::Lang
+{
+	class ASTNodeAssign final : public ASTNodeBase
+	{
+	public:
+		explicit ASTNodeAssign(Token token) noexcept;
+
+		void Accept(IASTNodeVisitor* pVisitor);
+
+		ASTNodeVariable* pHolder = nullptr;
+
+		ASTNodeExpression* pExpression = nullptr;
+	};
+}
